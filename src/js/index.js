ee4de91dfd6c7ac74f2d25a5bce728c8e99c45c6
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-04-01 20:06:57 -0300

    projeto finalizado

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const rightArrow = document.getElementById('right-arrow')
+const leftArrow = document.getElementById('left-arrow')
+
+
+const profile = document.getElementById('profile-info')
+const skills = document.getElementById('skills')
+
+rightArrow.addEventListener('click', () => {
+    profile.classList.add('hide')
+    rightArrow.classList.add('hide')
+    skills.classList.remove('hide')
+    leftArrow.classList.remove('hide')
+})
+
+leftArrow.addEventListener('click', () => {
+    profile.classList.remove('hide')
+    rightArrow.classList.remove('hide')
+    skills.classList.add('hide')
+    leftArrow.classList.add('hide')
+})
